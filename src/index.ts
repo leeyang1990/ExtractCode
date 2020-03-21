1a2275edb1d257829f42fdd8d5c55517b2cfff08
liyang <leeyang1990@gmail.com>
2020-03-21 16:00:26 +0800

    update lint

--- src/index.ts
+++ src/index.ts
@@ -3,90 +3,93 @@ import * as path from 'path';
 import walk from 'walk-sync';
 import stripJsonComments from 'strip-json-comments';
 const officegen = require('officegen');
-const config = JSON.parse(stripJsonComments(fs.readFileSync('src/config.json','utf-8')));
+const config = JSON.parse(stripJsonComments(fs.readFileSync('src/config.json', 'utf-8')));
 
 
 const root = config.project;//提取路径
 const globs = config.globs;//提取范围
 const temp = 'out/temp.txt';
-const excludeFileName:string[] = config.excludeFileName;//排除的文件，文件名
-const excludeFilePath:string[] = config.excludeFilePath;//排除的行，字符串包含
-const excludeLineText:string[] = config.excludeLineText;//排除的行，字符串包含
+const excludeFileName: string[] = config.excludeFileName;//排除的文件，文件名
+const excludeFilePath: string[] = config.excludeFilePath;//排除的行，字符串包含
+const excludeLineText: string[] = config.excludeLineText;//排除的行，字符串包含
 const headerText = config.headerText;
 const fontFace = config.fontFace;
 const fontSize = config.fontSize;
 async function run() {
-    if(!fs.existsSync(root))return;
-    let paths = await getAllFiles(root,globs);
-    paths = filterFileName(paths,excludeFileName);
-    paths = filterFilePath(paths,excludeFilePath);
+    if (!fs.existsSync(root)) {
+        console.error("project not exist");
+        return;
+    };
+    let paths = await getAllFiles(root, globs);
+    paths = filterFileName(paths, excludeFileName);
+    paths = filterFilePath(paths, excludeFilePath);
     fs.removeSync(temp);
     fs.ensureFileSync(temp);
-    let lines:string[] = [];
+    let lines: string[] = [];
     for (const e of paths) {
         const ct = formatFile2Content(e);
-        lines = lines.concat(filterLines(ct.split('\n'),excludeLineText));
-        fs.appendFileSync(temp,ct);
+        lines = lines.concat(filterLines(ct.split('\n'), excludeLineText));
+        fs.appendFileSync(temp, ct);
 
     }
-    const r1 = lines.slice(0,3101).join('\n');
+    const r1 = lines.slice(0, 3101).join('\n');
     genDoc(r1);
 }
 ///按特征文字过滤行数据
-export function filterLines(src:string[],ext:string[]){
-    return src.filter((e)=>ext.every((ee)=>!e.includes(ee)));
+export function filterLines(src: string[], ext: string[]) {
+    return src.filter((e) => ext.every((ee) => !e.includes(ee)));
 }
 ///按文件名排除
-export function filterFileName(paths:string[],exc:string[]){
-    return paths.filter((e)=> exc.indexOf(path.basename(e))===-1);
+export function filterFileName(paths: string[], exc: string[]) {
+    return paths.filter((e) => exc.indexOf(path.basename(e)) === -1);
 }
 ///按文件路径排除
-export function filterFilePath(paths:string[],excs:string[]){
-    return paths.filter((e)=>{
-        const excsMatch = excs.filter((exc)=>{
+export function filterFilePath(paths: string[], excs: string[]) {
+    return paths.filter((e) => {
+        const excsMatch = excs.filter((exc) => {
             return e.indexOf(exc) === 0;
         });
         return excsMatch.length === 0;
     });
 }
 ///获取文件列表
-async function getAllFiles(root:string,globs:string[]){
-    const paths = walk(root,{ globs: globs}).map((e)=>path.join(root,e)).map((e)=>e.split(path.sep).join('/')); 
+async function getAllFiles(root: string, globs: string[]) {
+    const paths = walk(root, { globs: globs }).map((e) => path.join(root, e)).map((e) => e.split(path.sep).join('/'));
     return paths;
 }
 ///读取文件并返回过滤空行和注释的内容
-function formatFile2Content(path:string){
-    let content = fs.readFileSync(path,'utf-8');
+function formatFile2Content(path: string) {
+    let content = fs.readFileSync(path, 'utf-8');
     content = replaceNote(content);
     content = replaceBlank(content);
     return content;
 }
-function replaceBlank(src:string){
+function replaceBlank(src: string) {
     const reg = /\n(\n)*( )*(\n)*\n/g;
     return src.replace(/(\n[\s\t]*\r*\n)/g, '\n').replace(/^[\n\r\n\t]*|[\n\r\n\t]*$/g, '');//src.replace(reg,'');
 }
-function replaceNote(src:string){
+function replaceNote(src: string) {
     const reg = /(\/\/.*)|(\/\*[\s\S]*?\*\/)/g;
-    return src.replace(reg,'');
+    return src.replace(reg, '');
 }
 ///生成doc
-function genDoc(src:string){
+function genDoc(src: string) {
     const docx = officegen({
-        type:'docx'
+        type: 'docx'
     });
-    docx.on('finalize', function(written: any) {
+    docx.on('finalize', function (written: any) {
         console.log(
-          'Finish to create a Microsoft Word document.'
+            'Finish to create a Microsoft Word document.'
         );
-      });
-    docx.on('error', function(err:any) {
+    });
+    docx.on('error', function (err: any) {
         console.log(err);
     });
     const pObj = docx.createP();
-    pObj.addText(src,{ font_face: fontFace, font_size: fontSize });
+    pObj.addText(src, { font_face: fontFace, font_size: fontSize });
 
     const header = docx.getHeader().createP();
-    header.addText ( headerText);
+    header.addText(headerText);
     //TODO:页眉加入页码
     header.options.align = 'left';
     const out = fs.createWriteStream('out/out.docx');
--- src/test.ts
+++ src/test.ts
@@ -1,40 +1,40 @@
 import assert from 'assert';
 import * as main from './index';
-describe('main', function() {
-  describe('#filterLines()', function() {
-    it('', ()=> {
-        const excludeLine = ['import '];
-        const src = [
-            `import * as fs from 'fs-extra';`,
-            `const officegen = require('officegen');`,
-            `import walk from 'walk-sync';`
-        ];
-        const result = main.filterLines(src,excludeLine);
+describe('main', function () {
+  describe('#filterLines()', function () {
+    it('', () => {
+      const excludeLine = ['import '];
+      const src = [
+        `import * as fs from 'fs-extra';`,
+        `const officegen = require('officegen');`,
+        `import walk from 'walk-sync';`
+      ];
+      const result = main.filterLines(src, excludeLine);
       assert.equal(result[0], `const officegen = require('officegen');`);
     });
   });
-  describe('#filterFileName()', function() {
-    it('', ()=> {
-        const excludeLine = ['a.ts','b.ts'];
-        const src = [
-            `C:/Users/pc/project/a.ts`,
-            `C:/Users/pc/project/b.ts`,
-            `C:/Users/pc/project/c.ts`
-        ];
-        const result = main.filterFileName(src,excludeLine);
+  describe('#filterFileName()', function () {
+    it('', () => {
+      const excludeLine = ['a.ts', 'b.ts'];
+      const src = [
+        `C:/Users/pc/project/a.ts`,
+        `C:/Users/pc/project/b.ts`,
+        `C:/Users/pc/project/c.ts`
+      ];
+      const result = main.filterFileName(src, excludeLine);
       assert.equal(result[0], `C:/Users/pc/project/c.ts`);
     });
   });
-  describe('#filterFilePath()', function() {
-    it('', ()=> {
-        const excludeLine = ['C:/Users/pc/project/a.ts','C:/Users/pc/project/b.ts','C:/Users/pc/project/d'];
-        const src = [
-            `C:/Users/pc/project/a.ts`,
-            `C:/Users/pc/project/b.ts`,
-            `C:/Users/pc/project/c.ts`,
-            `C:/Users/pc/project/d/d.ts`,
-        ];
-        const result = main.filterFilePath(src,excludeLine);
+  describe('#filterFilePath()', function () {
+    it('', () => {
+      const excludeLine = ['C:/Users/pc/project/a.ts', 'C:/Users/pc/project/b.ts', 'C:/Users/pc/project/d'];
+      const src = [
+        `C:/Users/pc/project/a.ts`,
+        `C:/Users/pc/project/b.ts`,
+        `C:/Users/pc/project/c.ts`,
+        `C:/Users/pc/project/d/d.ts`,
+      ];
+      const result = main.filterFilePath(src, excludeLine);
       assert.equal(result[0], `C:/Users/pc/project/c.ts`);
     });
   });
